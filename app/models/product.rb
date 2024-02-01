03c6d6bfdffa5186fb43d8f96b2f237afa97ebbd
Yondu Yondu <yonduapps@gmail.com>
2024-02-02 00:11:41 +0800

    Add product stocks module

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
-  belongs_to :category
   has_many_attached :images do |attachable|
-    attachable.variant :thumb, resize_to_limit: [50,50]
+    attachable.variant :thumb, resize_to_limit: [50, 50]
+    attachable.variant :medium, resize_to_limit: [250, 250]
   end
+
+  belongs_to :category
+  has_many :stocks
 end
