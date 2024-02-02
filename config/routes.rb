b60166c9616be6a68388e999b3d247485aecf192
Yondu Yondu <yonduapps@gmail.com>
2024-02-02 09:35:57 +0800

    Home index page with navbar and footer

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,8 @@ Rails.application.routes.draw do
     root to: "admin#index", as: :admin_root
   end
 
+  resources :categories, only: [:show]
+  resources :products, only: [:show]
+  
   get "admin" => "admin#index"
 end
